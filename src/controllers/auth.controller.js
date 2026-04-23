0e0fe8d8f9622ff3797eb668775b3d7499379b03
bibekshah220 <bibekshah425@gmail.com>
2026-04-23 22:32:50 +0545

    implement user registration controller with validation and JWT token generation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 const userModel = require('../models/user.model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 /**
  * @name registerUserController
@@ -7,12 +9,57 @@ const userModel = require('../models/user.model');
  */
 
 async function registerUserController(req, res) {
+    const {username, email, password} = req.body;
 
+    if(!username || !email || !password) {
+        return res.status(400).json({message: 'please provide username, email and password'});
+    }
+
+    const isUserAlreadyExist = await userModel.findOne({
+        $or: [
+            {username},
+            {email}
+        ]   
+    })  
+
+    if(isUserAlreadyExist) {
+        return res.status(400).json({message: 'Account already exists with this username or email'});
+    }
+
+const hash = await bcrypt.hash(password, 10);
+const user = await userModel.create({
+    username,
+    email,
+    password: hash
+});
+
+const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '1d'});
+
+res.cookie('token', token, )
+
+
+res.status(201).json({
+    message: 'User registered successfully',
+     user:{
+        id: user._id,
+        username: user.username,
+        email: user.email
+     }
+    
+    
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+    
+module.exports = { registerUserController };
 }
-
-
-
-
-
-
-
