6109b7c95d0610d87805404c930e5dbd54d16efa
bibekshah220 <bibekshah425@gmail.com>
2026-04-23 21:31:06 +0545

    add database connection setup and integrate with server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-required 
+require("dotenv").config();
 
 const app  = require('./src/app');
 
+const connectDB = require('./src/config/database');
 
+connectDB();
 
 
 app.listen(8080, () => {
---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+async function connectDB() {
+try{
+await mongoose.connect(process.env.MONGO_URI)
+    console.log('Connected to Database');
+}
+  catch(error){
+    console.error(error);
+  }  
+}
+
+
+module.exports = connectDB;
