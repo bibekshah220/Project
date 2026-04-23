956675c228dfee7dfda2dc0fff72db3974e3f04e
bibekshah220 <bibekshah425@gmail.com>
2026-04-23 22:10:14 +0545

    add authentication routes and controller for user registration

--- src/app.js
+++ src/app.js
@@ -3,8 +3,15 @@ const express = require('express');
 const app = express();
 
 
+// required all the routes here
+const authRouter = require('./routes/auth.routes');
 
+
+
+// using all the routes here
 app.use(express.json());
 
+
+app.use('/api/auth', authRouter);
 
 module.exports = app;
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+const userModel = require('../models/user.model');
+
+/**
+ * @name registerUserController
+ * @description register a new user, expacts username, email and password in the request body
+ * @access Public
+ */
+
+async function registerUserController(req, res) {
+
+}
+
+
+
+
+
+
+
