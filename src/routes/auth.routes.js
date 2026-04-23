e9de8db5f932e34016b1c80dd9139a3aefeb8e1b
bibekshah220 <bibekshah425@gmail.com>
2026-04-23 22:42:44 +0545

    add login user functionality with JWT token generation and cookie handling

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,8 +7,15 @@ const authRouter = Router();
 * @desc Register a new user
 * @access Public
 */
-authRouter.post('/register', authController.registerUserController);
+authRouter.post('/register', authController.registerUserController,);
 
 
+/**
+ * @route POST /api/auth/login
+ * @desc Login a user
+ * @access Public
+ */
+authRouter.post('/login', authController.loginUserController,); 
+ 
 
 module.exports = authRouter;
